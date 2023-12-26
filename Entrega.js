60a28580b4be4833ad504c04181dda5971d8066f
Abel Hernan Reyes <abel.hernan.r@gmail.com>
2023-12-25 22:52:54 -0300

    first commit

---
+++ Entrega.js
@@ -0,0 +1,35 @@
+class ProductManager {
+    constructor(){
+        this.product=[]
+    }
+
+    getProduct(){
+       return this.product
+    }
+
+    getProductByID(id){
+        let result = this.product.find(product=>product.id==id)
+        return result=='undefine' ? 'no se encuentra el producto': result
+    }
+
+    addProduct(title,description,price,thumbnail,code,stock){
+        if (condition) {
+            const newProduct = {
+                id: this.product.length+1,
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            }
+            this.product.push(newProduct) 
+        }
+    }
+}
+
+const manager = new ProductManager();
+
+
+
+
